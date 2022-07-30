f8d48aa8b6f5224a236987a69c18f091d06a381e
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-30 12:49:24 -0500

    feature: Change primary color and secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,22 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      primary: '#001833',
-      secondary: '#003329',
-      neutral: '#AFB2B6',
+      primary: {
+        500: '#0077FF',
+        600: '#005FCC',
+        700: '#004799',
+        800: '#003066',
+        900: '#001833',
+        950: '#000C1A'
+      },
+      secondary: {
+        500: '#24DBB7',
+        600: '#00CCA3',
+        700: '#00997A',
+        800: '#006652',
+        900: '#003329',
+        950: '#001A14'
+      },
       white: '#FFF',
     },
     extend: {},
