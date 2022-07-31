2f4617bb39c62fcd0d9ea0b2f8739a671cefa3c7
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-31 16:31:36 -0500

    feat: create Slider component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,18 @@
 import React from 'react';
+import Slider from 'components/organisms/Slider';
 // import resume from 'data/profile.json';
 // import Header from 'components/organisms/Header';
 // import Summary from 'components/organisms/Summary';
 // import Experience from 'components/organisms/Experience';
 // import Projects from 'components/organisms/Project';
 // import Skills from 'components/organisms/Skills';
-import profile from 'assets/images/profile.jpg';
 
-export default function Home() {
+function Home() {
   return (
     <main>
-      <div className='art-info-bar'>
-        <div className='art-info-bar-frame'>
-          <div className='art-header'>
-            <div className='art-avatar'>
-              <a className='art-avatar-curtain'>
-                <img
-                  className='rounded-full mx-auto w-32 xl:w-64'
-                  src={profile}
-                  alt='profile'
-                />
-              </a>
-              <div className='art-lamp-light'>
-                <div className='art-available-lamp' />
-              </div>
-            </div>
-            <h5 className='art-name mb-10'>
-              <a href='home.html'>Artur Carter</a>
-            </h5>
-            <div className='art-sm-text'>
-              Front-end Deweloper Ui/UX Designer
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='art-content'>
-        <div className='art-curtain'>
-          <div className='art-top-bg'>
-            <div className='art-top-bg-overlay' />
-          </div>
-        </div>
-      </div>
+      <Slider />
     </main>
   );
 }
+
+export default Home;
