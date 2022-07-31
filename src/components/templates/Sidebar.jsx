28b579de5ff6756142516efe596b08c5b25f3798
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-31 17:28:22 -0500

    refactor: Add components idioms and Information to sidebar

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Avatar from 'components/molecules/Avatar';
+import Information from 'components/molecules/Information';
+import Idioms from 'components/molecules/Idioms';
 import resumen from 'data/sidebar.json';
 
 const Sidebar = () => (
   <div className='art-info-bar'>
     <div className='art-info-bar-frame'>
-      <div className='art-info-bar-header'>
-        <div className='art-info-bar-btn'>
-          <i className='fas fa-ellipsis-v' />
-        </div>
-      </div>
       <Avatar data={resumen.avatar} />
       <div
         id='scrollbar2'
@@ -18,37 +15,9 @@ const Sidebar = () => (
         tabIndex='-1'
       >
         <div className='scroll-content'>
-          <div className='art-table p-15-15'>
-            <ul>
-              <li>
-                <h6>Residence:</h6>
-                <span>Canada</span>
-              </li>
-
-              <li>
-                <h6>City:</h6>
-                <span>Toronto</span>
-              </li>
-
-              <li>
-                <h6>Age:</h6>
-                <span>26</span>
-              </li>
-            </ul>
-          </div>
+          <Information data={resumen.information} />
           <div className='art-ls-divider' />
-          <div className='art-lang-skills p-30-15'>
-            <div className='art-lang-skills-item'>
-              <h6>French</h6>
-            </div>
-            <div className='art-lang-skills-item'>
-              <h6>English</h6>
-            </div>
-            <div className='art-lang-skills-item'>
-              <h6>Spanish</h6>
-            </div>
-          </div>
-
+          <Idioms data={resumen.idioms} />
           <div className='art-ls-divider' />
 
           <ul className='art-knowledge-list p-15-0'>
