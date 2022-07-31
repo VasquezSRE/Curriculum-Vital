f86650ddae7c2b83db37a527e670ba7a3d0196eb
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-31 16:30:57 -0500

    feat: Apply template for apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import 'assets/styles/layout.scss';
+import Layout from 'components/templates/Layout';
+import Sidebar from 'components/templates/Sidebar';
+import Content from 'components/templates/Content';
 import Home from 'pages/Home';
-import Layout from 'layouts/Layout';
 
 function App() {
   return (
     <div className='App'>
       <Layout>
-        <Home />
+        <Sidebar />
+        <Content>
+          <Home />
+        </Content>
       </Layout>
     </div>
   );
