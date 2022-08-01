a7790dee4e99afa6cd98ea1c3b6dcfdd9900164b
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-31 20:07:05 -0500

    feat: Create component certifications

---
+++ src/components/atoms/certifications.jsx
@@ -0,0 +1,13 @@
+import cloud_practitioner from 'assets/images/certifications/aws-certified-cloud-practitioner.png';
+import business_accredited from 'assets/images/certifications/aws-partner-accreditation-business.png';
+import technical_accredited from 'assets/images/certifications/aws-partner-accreditation-technical.png';
+import cloud_economic_accredited from 'assets/images/certifications/aws-partner-cloud-economics-accreditation.png';
+
+const certifications = {
+  cloud_practitioner,
+  business_accredited,
+  technical_accredited,
+  cloud_economic_accredited,
+};
+
+export default certifications;
