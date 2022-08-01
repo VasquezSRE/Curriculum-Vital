d3eb709175fa7c02a1e7fbc864dbce43eaf3e558
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-08-01 07:29:06 -0500

    feat: Create Footer Component

---
+++ src/components/organisms/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='mx-auto sm:px-4 max-w-full mx-auto sm:px-4'>
+      <footer>
+        <div className='art-copy'>© 2022 - Andrés Vásquez</div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
