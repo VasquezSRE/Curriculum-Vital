6e602f629e590eeb101825840879f702fb89ca82
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-07-31 20:46:47 -0500

    feat: Create component for hobbies

--- src/components/atoms/hobbies.jsx
+++ src/components/atoms/hobbies.jsx
@@ -2,11 +2,10 @@ import uwr from 'assets/images/hobbies/uwr.png';
 import trekking from 'assets/images/hobbies/trekking.png';
 import pingpong from 'assets/images/hobbies/pingpong.png';
 
-
 const hobbies = {
   uwr,
   trekking,
-  pingpong
+  pingpong,
 };
 
 export default hobbies;
