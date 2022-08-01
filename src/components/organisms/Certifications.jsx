6ac6846786ca344dff004880324796e5857f061b
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-08-01 07:11:30 -0500

    fix: Add title component certifications

--- src/components/organisms/Certifications.jsx
+++ src/components/organisms/Certifications.jsx
@@ -4,6 +4,13 @@ import certifications from 'components/atoms/certifications';
 const Certifications = ({ data }) => (
   <div className='container mx-auto sm:px-4 max-w-full mx-auto sm:px-4'>
     <div className='flex flex-wrap '>
+      <div className='lg:w-full pr-4 pl-4'>
+        <div className='art-section-title'>
+          <div className='art-title-frame'>
+            <h4>Certificaciones</h4>
+          </div>
+        </div>
+      </div>
       {data.certifications.map(({ image }) => (
         <div key={image} className='w-1/2 lg:w-1/4 pr-4 pl-4'>
           <img className='art-brand' src={certifications[image]} alt='brand' />
